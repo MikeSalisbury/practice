857e2f07222afee85e9340dac85d7f254f3aa314
Mike Salisbury <michael.salisbury87@gmail.com>
2018-03-14 09:57:30 -0700

    gamma_fnc completed

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -9,10 +9,6 @@ console.log(sum_to(5) === 15);
 
 // 2) Write a function add_numbers(nums_array) that takes in an array
 // of Fixnums and returns the sum of those numbers. Write this method recursively.
-function add_numbers(nums_array) {
-  if (nums_array.length < 1) return 0;
-  return nums_array[0] + add_numbers(nums_array[1..nums_array.length]);
-}
 
 function add_numbers(nums_array) {
   if (nums_array.length < 1) return 0;
@@ -20,3 +16,16 @@ function add_numbers(nums_array) {
 }
 
 console.log(add_numbers([6, 3, 5, 8, 9]) === 31);
+
+// 3) Let's write a method that will solve Gamma Function recursively.
+// The Gamma Function is defined Γ(n) = (n-1)!.
+function gamma_fnc(n) {
+  if (n < 1) return null;
+  if (n === 1) return 1;
+  let result = n-1;
+  return result * gamma_fnc(result);
+}
+
+console.log(gamma_fnc(1) === 1);
+console.log(gamma_fnc(4) === 6);
+console.log(gamma_fnc(8) === 5040);
